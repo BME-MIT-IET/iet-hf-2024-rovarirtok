a7827bb5fa162e5d086041f15042f9e577ae23a7
Andras Palasti <andraspalasti2@gmail.com>
2024-04-19 18:08:44 +0200

    basic test case added

---
+++ src/test/java/GUI/UITest.java
@@ -0,0 +1,11 @@
+package GUI;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class UITest {
+    @Test
+    public void demoTest() {
+        assertEquals(6, 3+3);
+    }
+}
